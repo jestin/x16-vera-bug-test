d9333e0172234cd0e7a8530a03b3d45f925f6606
Jestin Stoffel <jestin.stoffel@gmail.com>
2022-09-17 10:56:37 -0500

    Add 16x8 8bpp

--- main.asm
+++ main.asm
@@ -107,6 +107,16 @@ exeebpp_pal_file: .literal "8X88BPP.PAL"
 end_exeebpp_pal_file:
 
 
+sxeebpp_tile_file: .literal "16X88BPPT.BIN"
+end_sxeebpp_tile_file:
+
+sxeebpp_map_file: .literal "16X88BPPM.BIN"
+end_sxeebpp_map_file:
+
+sxeebpp_pal_file: .literal "16X88BPP.PAL"
+end_sxeebpp_pal_file:
+
+
 vram_tiles = $00000
 vram_l0_map = $10000
 vram_pal = $1fa00
@@ -227,6 +237,11 @@ tick:
 	cmp #8
 	bne :+
 	jsr load_8x8_8bpp
+:
+	lda zp_mode
+	cmp #9
+	bne :+
+	jsr load_16x8_8bpp
 :
 
 	lda #1
@@ -865,6 +880,76 @@ load_8x8_8bpp:
 	lda #(end_exeebpp_pal_file-exeebpp_pal_file)
 	ldx #<exeebpp_pal_file
 	ldy #>exeebpp_pal_file
+	jsr SETNAM
+	lda #(^vram_pal + 2)
+	ldx #<vram_pal
+	ldy #>vram_pal
+	jsr LOAD
+
+	rts
+
+;==================================================
+; load_16x8_8bpp
+;==================================================
+load_16x8_8bpp:
+	; set video mode
+	lda #%00010001		; l0 enabled
+	sta veradcvideo
+
+	; set the l0 tile mode	
+	lda #%00000011 	; height (2-bits) - 0 (32 tiles)
+					; width (2-bits) - 0 (32 tiles
+					; T256C - 0
+					; bitmap mode - 0
+					; color depth (2-bits) - 3 (8bpp)
+	sta veral0config
+
+	lda #(<(vram_tiles >> 9) | (0 << 1) | 1)
+								;  height    |  width
+	sta veral0tilebase
+	
+	; set the tile map base address
+	lda #<(vram_l0_map >> 9)
+	sta veral0mapbase
+
+	; set video scale to 2x
+	lda #64
+	sta veradchscale
+	sta veradcvscale
+
+	lda #1
+	ldx #8
+	ldy #0
+	jsr SETLFS
+	lda #(end_sxeebpp_tile_file-sxeebpp_tile_file)
+	ldx #<sxeebpp_tile_file
+	ldy #>sxeebpp_tile_file
+	jsr SETNAM
+	lda #(^vram_tiles + 2)
+	ldx #<vram_tiles
+	ldy #>vram_tiles
+	jsr LOAD
+
+	lda #1
+	ldx #8
+	ldy #0
+	jsr SETLFS
+	lda #(end_sxeebpp_map_file-sxeebpp_map_file)
+	ldx #<sxeebpp_map_file
+	ldy #>sxeebpp_map_file
+	jsr SETNAM
+	lda #(^vram_l0_map + 2)
+	ldx #<vram_l0_map
+	ldy #>vram_l0_map
+	jsr LOAD
+
+	lda #1
+	ldx #8
+	ldy #0
+	jsr SETLFS
+	lda #(end_sxeebpp_pal_file-sxeebpp_pal_file)
+	ldx #<sxeebpp_pal_file
+	ldy #>sxeebpp_pal_file
 	jsr SETNAM
 	lda #(^vram_pal + 2)
 	ldx #<vram_pal
